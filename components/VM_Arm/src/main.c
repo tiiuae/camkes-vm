38fcf8f49c0f68774b1dea9309c4da858f27145f
Joonas Onatsu <joonas.onatsu@unikie.com>
2022-10-19 02:00:03 +0300

    vm-arm: Add memory debug option
    Add config option and debug prints for
    debugging/monitoring CAmkES stage-2
    memory mapping shenanigans.
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>
    
    trivial: Fix debug print to use long int
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>
    
    trivial: Fix style
    
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- components/VM_Arm/src/main.c
+++ components/VM_Arm/src/main.c
@@ -5,11 +5,15 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#define ZF_LOG_LEVEL ZF_LOG_INFO
-
 #include <autoconf.h>
 #include <arm_vm/gen_config.h>
 #include <sel4muslcsys/gen_config.h>
+
+#ifdef CONFIG_ARM_VM_CAMKES_MEMORY_DEBUG
+#define ZF_LOG_LEVEL ZF_LOG_VERBOSE
+#else
+#define ZF_LOG_LEVEL ZF_LOG_INFO
+#endif
 
 #include <stdio.h>
 #include <assert.h>
@@ -445,6 +449,14 @@ static int vmm_init(void)
     utspace_alloc_at_copy = vka->utspace_alloc_at;
     vka->utspace_alloc_at = camkes_vm_utspace_alloc_at;
 
+#ifdef CONFIG_ARM_VM_CAMKES_MEMORY_DEBUG
+    const char *uttypes[3] = {
+        [ALLOCMAN_UT_KERNEL]  = "ALLOCMAN_UT_KERNEL",
+        [ALLOCMAN_UT_DEV]     = "ALLOCMAN_UT_DEV",
+        [ALLOCMAN_UT_DEV_MEM] = "ALLOCMAN_UT_DEV_MEM"
+    };
+#endif
+
     for (int i = 0; i < simple_get_untyped_count(simple); i++) {
         size_t size;
         uintptr_t paddr;
@@ -457,6 +469,10 @@ static int vmm_init(void)
             paddr >= ram_paddr_base && paddr <= (ram_paddr_base + (ram_size - 1))) {
             utType = ALLOCMAN_UT_DEV_MEM;
         }
+#ifdef CONFIG_ARM_VM_CAMKES_MEMORY_DEBUG
+        ZF_LOGV("Adding simple untyped (%d) to allocman: cap = %ld, size = 0x%lx, paddr = %p, type = %s", i, cap, BIT(size),
+                (void *) paddr, uttypes[utType]);
+#endif
         err = allocman_utspace_add_uts(allocman, 1, &path, &size, &paddr, utType);
         assert(!err);
     }
@@ -473,6 +489,10 @@ static int vmm_init(void)
                 paddr <= (ram_paddr_base + (ram_size - 1))) {
                 utType = ALLOCMAN_UT_DEV_MEM;
             }
+#ifdef CONFIG_ARM_VM_CAMKES_MEMORY_DEBUG
+            ZF_LOGV("Adding CAmkES untyped (%d) to allocman: cap = %ld, size = 0x%lx, paddr = %p, type = %s", i, cap, BIT(size),
+                    (void *) paddr, uttypes[utType]);
+#endif
             err = allocman_utspace_add_uts(allocman, 1, &path, &size, &paddr, utType);
             assert(!err);
         }
@@ -1097,7 +1117,7 @@ memory_fault_result_t unhandled_mem_fault_callback(vm_t *vm, vm_vcpu_t *vcpu,
                 return rc;
             }
         }
-        reservation = vm_reserve_memory_at(vm, addr, SIZE_BITS_TO_BYTES(seL4_PageBits),
+        reservation = vm_reserve_memory_at(vm, addr, PAGE_SIZE_4K,
                                            handle_on_demand_fault_callback, NULL);
         mapped = vm_map_reservation(vm, reservation, on_demand_iterator, (void *)vm);
         if (!mapped) {
