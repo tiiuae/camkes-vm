8f9f2d0ab4eb5ac9c9fb33de5635f3b0bd8a074c
Joonas Onatsu <joonas.onatsu@unikie.com>
2022-11-13 02:46:59 +0200

    interrupts/wip: Update BCM2711 IRQs
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

--- components/VM_Arm/plat_include/bcm2711/plat/vmlinux.h
+++ components/VM_Arm/plat_include/bcm2711/plat/vmlinux.h
@@ -8,6 +8,10 @@
 /*
  * BCM2711 TRM
  * section 6.3.   GIC-400: SPI IDs start from 32
+ * section 6.5.1  
+ *                GIC-400 ARM base address: 0x4c0040000
+ *                GIC-400 Low-Peripheral mode base address: 0xff840000
+ *                GIC-400 is configured with: "NUM_CPUS=4", "NUM_SPIS=192"
  */
 #define IRQ_SPI_OFFSET 32
 #define GIC_NODE_PATH "/soc/interrupt-controller@40041000"
@@ -27,7 +31,28 @@ static const int linux_pt_irqs[] = {};
  *    Software interrupt 6 = 78
  *    Software interrupt 7 = 79
  */
-static const int free_plat_interrupts[] =  { 72, 73, 74, 75, 76, 77, 78, 79 };
+
+/* 
+ * Although using the software interrupts (SGIs?) listed above seem to work,
+ * I don't know if using them is the intended use case for this. Any documentation
+ * I can find doesn't really elaborate anything on how to use the SGIs.
+ * 
+ * So, the Ultibo project has done a whole lot on RPi4 support, and a RPi4 SoC
+ * definitions can be found here (BCM2711 is based on BCM2838):
+ * https://github.com/ultibohub/Core/blob/master/source/rtl/ultibo/core/bcm2838.pas
+ * 
+ * The last interrupt listed there is the xHCI controller IRQ, IRQ_SPI_OFFSET + 176.
+ * Based on that and the fact that "NUM_SPIS" is set to 192, we should have a whole
+ * lot of IRQs to use which don't correspond to any HW IRQ, SPI IDs 177->192.
+ * Don't know if this is correct either, as the documentation sucks, but this also
+ * seems to work.
+ */
+static const int free_plat_interrupts[] =  { IRQ_SPI_OFFSET + 177,
+                                             IRQ_SPI_OFFSET + 178,
+                                             IRQ_SPI_OFFSET + 179,
+                                             IRQ_SPI_OFFSET + 180,
+                                             IRQ_SPI_OFFSET + 181 };
+//static const int free_plat_interrupts[] =  { 72, 73, 74, 75, 76, 77, 78, 79 };
 static const char *plat_keep_devices[] = {
     "/timer",
     "/soc/interrupt-controller@40041000",
